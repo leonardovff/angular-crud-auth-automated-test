76a91ba141c853ac8335417751bea3f886055b83
Leonardo Victor <leonardovff@gmail.com>
2019-08-12 01:01:40 -0300

    add cpf mask directive to project

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,27 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { ListComponent } from './components/list/list.component'
 import { ListItemComponent } from './components/list/list-item/list-item.component'
 import { RouterModule } from '@angular/router';
 import { ClientInterceptor } from './interceptors/client.interceptor';
+import { CpfMaskDirective } from './directives/cpf-mask.directive';
 
 
 @NgModule({
   declarations: [
     ListComponent,
-    ListItemComponent
+    ListItemComponent,
+    CpfMaskDirective
   ],
   imports: [
     CommonModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   exports: [
+    ReactiveFormsModule,
+    HttpClientModule,
     ListComponent,
     ListItemComponent,
-    HttpClientModule
+    CpfMaskDirective
   ],
   providers: [
     {
