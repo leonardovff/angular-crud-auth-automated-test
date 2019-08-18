a5b0496768e3003e79e8de10d1c737400f45ac58
Leonardo Victor <leonardovff@gmail.com>
2019-08-17 21:26:13 -0300

    add more one behavior in client form test

--- src/app/modules/clients/components/client-form/client-form.component.spec.ts
+++ src/app/modules/clients/components/client-form/client-form.component.spec.ts
@@ -23,8 +23,8 @@ class mockVehicleService {
   constructor(){
     setTimeout(() => {
       this.brands.next(VehicleBrands.map(vehicle => ({
+        id: vehicle.id,
         description: vehicle.description,
-        id: vehicle.id
       })));
     }, 100);
   }
@@ -41,6 +41,15 @@ describe('ClientFormComponent', () => {
   let spyClientServiceUpdate;
   let spyClientServiceGet;
   let mockActivatedRoute;
+
+  const fillAndValidField = async (inputId, callback) => {
+    const formControl = component.form.get(inputId);
+    const el = fixture.debugElement.query(By.css('#'+inputId)).nativeElement;
+    await callback(el, formControl);
+    fixture.detectChanges();
+    expect(formControl.valid).toBeTruthy(`input ${inputId} validation`);
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -116,17 +125,10 @@ describe('ClientFormComponent', () => {
     fixture.detectChanges();
     expect(component.form.valid).toBeFalsy("form validation");
 
-    const fillAndValidField = async (inputId, callback) => {
-      const formControl = component.form.get(inputId);
-      const el = fixture.debugElement.query(By.css('#'+inputId)).nativeElement;
-      expect(formControl.valid).toBeFalsy(`input ${inputId} validation`);
-      await callback(el);
-      fixture.detectChanges();
-      expect(formControl.valid).toBeTruthy(`input ${inputId} validation`);
-    }
     for (const input in arrage.input.client) {
       const value = arrage.input.client[input];
-      await fillAndValidField(input, async el => {
+      await fillAndValidField(input, async (el, control) => {
+        expect(control.valid).toBeFalsy(`input ${input} validation`);
         if(input == 'vehicle_brand' || input == 'vehicle_model'){
           await new Promise(res => setTimeout(() => (res()),200));
           fixture.detectChanges();
@@ -140,5 +142,129 @@ describe('ClientFormComponent', () => {
     expect(component.form.valid).toBeTruthy("form validation");
     fixture.debugElement.query(By.css('button')).nativeElement.click();
     expect(spyClientServiceCreate).toHaveBeenCalledWith(arrage.expect.client);
+  });
+  it('should edit one client', async () => {
+    let arrage = {
+      input: {
+        params: {id: 1},
+        id: 1,
+        message: "Salvo com sucesso",
+        returnUpdateRequest: {"message": "Success"},
+        change: {
+          name: "Michael Ratliff2",
+          vehicle_brand: 4,
+          vehicle_model: 21
+        },
+        clientRequest: {
+          name: "Michael Ratliff",
+          cpf: "198.317.840-30",
+          phone: "(83) 99893-4192 ",
+          birth_date: "11/06/1972",
+          address: "Herkimer Court, 492",
+          vehicle_brand: {
+            id: 2,
+            description: "Agrale",
+          },
+          vehicle_model: {
+            id: 4567,
+            description: "MARRU\u00c1 AM 200 2.8  CD TDI Diesel",
+          },
+        },
+      },
+      expect: {
+        id: 1,
+        message: "Salvo com sucesso",
+        client: {
+          name: "Michael Ratliff2",
+          cpf: "198.317.840-30",
+          phone: "(83) 99893-4192",
+          birth_date: "11/06/1972",
+          address: "Herkimer Court, 492",
+          vehicle_brand: {
+            id: 4,
+            description: "AM Gen",
+          },
+          vehicle_model: {
+            id: 21,
+            description: "Hummer Hard-Top 6.5 4x4 Diesel TB",
+          },
+        }
+      }
+    }
+    spyClientServiceUpdate.and.returnValue(of(arrage.input.returnUpdateRequest).toPromise());
+    spyClientServiceGet.and.returnValue(of(arrage.input.clientRequest).toPromise())
+    mockActivatedRoute.snapshot.params = arrage.input.params;
+    fixture.detectChanges();
+
+    expect(component.form.valid).toBeFalsy("form validation");
+
+    expect(spyClientServiceGet).toHaveBeenCalledWith(arrage.input.params.id);
+    await spyClientServiceGet.calls.mostRecent().returnValue;
+    //Update fields after get response
+    fixture.detectChanges();
+
+    for (const input in arrage.input.change) {
+      const value = arrage.input.change[input];
+      await fillAndValidField(input, async (el, control) => {
+        if(input == "vehicle_model"){
+          expect(control.valid).toBeFalsy(`input ${input} validation after changed the vehicle brand`);
+        } else {
+          expect(control.valid).toBeTruthy(`input ${input} validation`);
+        }
+        if(input == 'vehicle_brand' || input == 'vehicle_model'){
+          await new Promise(res => setTimeout(() => (res()),200));
+          fixture.detectChanges();
+          el.value = value;
+          return el.dispatchEvent(new Event('change'));
+        }
+        el.value = value;
+        el.dispatchEvent(new Event('input'));
+      });
+    }
+    expect(component.form.valid).toBeTruthy("form validation");
+    fixture.debugElement.query(By.css('button')).nativeElement.click();
+    fixture.detectChanges();
+    expect(spyClientServiceUpdate).toHaveBeenCalledWith(arrage.expect.id, arrage.expect.client);
+  });
+  it('should not create one client with wrong data', async () => {
+    let arrage = {
+      input: {
+        params: {},
+        message: "Salvo com sucesso",
+        returnRequestCreate: {"message": "Error"},
+        client: {
+          name: "Guilherme Silveira",
+          cpf: "74757602863",
+          phone: "8593-7412",
+          birth_date: "23041961",
+          address: "Rua Machado de Assis",
+          vehicle_brand: 2,
+          vehicle_model: 4564,
+        }
+      }
+    }
+    spyClientServiceCreate.and.returnValue(of(arrage.input.returnRequestCreate).toPromise());
+    mockActivatedRoute.snapshot.params = arrage.input.params;
+    fixture.detectChanges();
+    expect(component.form.valid).toBeFalsy("form validation");
+
+    for (const input in arrage.input.client) {
+      const value = arrage.input.client[input];
+      const control = component.form.get(input);
+      const el = fixture.debugElement.query(By.css('#'+input)).nativeElement;
+      if(input == 'vehicle_brand' || input == 'vehicle_model'){
+        await new Promise(res => setTimeout(() => (res()),200));
+        fixture.detectChanges();
+        el.value = value;
+        el.dispatchEvent(new Event('change'));
+        continue;
+      }
+      el.value = value;
+      el.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+    }
+    expect(component.form.valid).toBeFalsy("form validation");
+    fixture.debugElement.query(By.css('button')).nativeElement.click();
+    expect(spyClientServiceCreate).not.toHaveBeenCalled()
   });
 });
