137db6678b88cae93ac83a7e8a8db4b6992c1b0c
Roberto Barbosa <roberto@robertos-air.localdomain>
2019-09-19 22:04:28 -0300

    addinger interceptor user

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,7 @@
+export interface UserInterface {
+  id: number;
+  name: string;
+  cpf: string;
+  email: string;
+  password: string;
+}
---
+++ src/app/modules/shared/interceptors/user-data.ts
@@ -0,0 +1,25 @@
+import { UserInterface } from 'src/app/interfaces/user.interface';
+
+export let UsersData: Array<UserInterface> = [
+  {
+    id: 1,
+    name: 'Lora Middleton',
+    cpf: '173.676.730-50',
+    email: 'lora@lora.com',
+    password: '123123'
+  },
+  {
+    id: 2,
+    name: 'Christian Mcguire',
+    cpf: '975.073.450-50',
+    email: 'chris@chris.com',
+    password: '1234566'
+  },
+  {
+    id: 3,
+    name: 'Berta Mejia',
+    cpf: '173.676.730-50',
+    email: 'berta@berta.com',
+    password: '123456'
+  }
+];
